588a2be7e05b11ff25367066b92baaf34bdb4b57
AleksandrKrivoruchko <avk741120@yandex.ru>
2023-03-11 13:10:20 +0300

    Trial revision of the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,19 @@
 import connectDb.ConnectSqlite;
 import connectDb.IConnectDb;
-import model.Animal;
-import model.IAnimal;
-import model.PetAnimal;
+//import model.IAnimal;
+//import model.PetAnimal;
 
-import java.sql.*;
-import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args) {
-        IAnimal animal = new PetAnimal("кошка", "Дуся", "02.11.2016");
-        System.out.println(animal.formatDate());
-        System.out.println(animal.getAge());
+//        IAnimal animal = new PetAnimal("кошка", "Дуся", "02.11.2016");
+//        System.out.println(animal.formatDate());
+//        System.out.println(animal.getAge());
         IConnectDb cs = new ConnectSqlite("my_database.db", "animal_pet");
         cs.connectDb();
 //       cs.createTable();
-        cs.add(animal);
+//        cs.add(animal);
         System.out.println(cs.select());
 //        cs.update(1, animal);
 //        System.out.println(cs.select());
